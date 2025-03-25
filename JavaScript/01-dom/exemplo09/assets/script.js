8a5b2a5a9801e527fcefa5ac7be358380fc7af7a
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-03-25 16:32:24 -0300

    Start

--- JavaScript/01-dom/exemplo09/assets/script.js
+++ JavaScript/01-dom/exemplo09/assets/script.js
@@ -7,10 +7,17 @@ document.querySelector("#content").innerHTML =
       <li>Desenvolvimento de Aplicações</li>
      </ul>`;
 
-console.log(document.querySelector("h1"));
+/*console.log(document.querySelector("h1"));
 console.log(document.querySelector("#content"));
-
 document.querySelector("h1").remove();
 document.querySelector("#content").remove();
+console.log(document.querySelector("h1"));*/
 
-console.log(document.querySelector("h1"));
+console.log(document.querySelector("span"));
+const spanBox = document.querySelector("span");
+console.log(spanBox);
+spanBox.insertAdjacentHTML("beforebegin","<span class='red'>Antes do Ínicio</span>");
+spanBox.insertAdjacentHTML("afterend","<span class='red'>Depois do Final</span>");
+
+spanBox.insertAdjacentHTML("afterbegin","<span class='red'>Depois do Ínicio</span>");
+spanBox.insertAdjacentHTML("beforeend","<span class='red'>Antes do Final</span>");
