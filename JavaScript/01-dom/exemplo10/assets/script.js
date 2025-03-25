1bd6763b699acd7a4ea23c39e7d5d2a11d222bdf
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-03-25 17:03:16 -0300

    Manipulando o DOM

---
+++ JavaScript/01-dom/exemplo10/assets/script.js
@@ -0,0 +1,21 @@
+
+const body = document.querySelector("body");
+const titleH1 = document.createElement("h1");
+titleH1.textContent = "Disciplinas";
+console.log(titleH1);
+body.insertAdjacentElement("afterbegin",titleH1);
+
+const divBox = document.createElement("div");
+divBox.id = "box";
+console.log(divBox);
+body.insertAdjacentElement("beforeend",divBox);
+
+const spanTitle = document.createElement("span");
+spanTitle.id = "title";
+spanTitle.textContent = "CAIXA";
+const spanContent = document.createElement("span");
+spanContent.id = "content";
+spanContent.textContent = "Lista";
+
+divBox.insertAdjacentElement("afterbegin",spanTitle);
+divBox.insertAdjacentElement("beforeend",spanContent);
