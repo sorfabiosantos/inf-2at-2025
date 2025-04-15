da28519764e0461cb27dc3c4b915773cb23b1d58
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-04-15 17:58:14 -0300

    Vetores, Matrizes e Objetos

--- JavaScript/03-vetores-matrizes-objetos/exercicios/exercicio01/assets/script.js
+++ JavaScript/03-vetores-matrizes-objetos/exercicios/exercicio01/assets/script.js
@@ -1,3 +1,14 @@
 console.log("Exercício 01");
 
 const myArrayNames = [];
+let i = 0;
+console.log(myArrayNames);
+
+const button = document.querySelector("button");
+const inputName = document.querySelector("input");
+
+button.addEventListener("click", () => {
+    myArrayNames[i] = inputName.value;
+    console.log(myArrayNames);
+    i++;
+});
