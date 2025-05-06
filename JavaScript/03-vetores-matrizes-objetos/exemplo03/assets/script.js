9d8eba4e9f8d11ff2880b52590f1be9ef1530648
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-05-06 17:16:31 -0300

    Vetores, Matrizes e Objetos - Exercícios

--- JavaScript/03-vetores-matrizes-objetos/exemplo03/assets/script.js
+++ JavaScript/03-vetores-matrizes-objetos/exemplo03/assets/script.js
@@ -25,12 +25,23 @@ const pessoas = [
         email: 'guilhermecardoso@gmail.com',
     },
 ];
-
+/*
 const array = [1, 2, 3, 4, 5, 6];
 console.log(array);
+
 array.forEach((value,index,vet) => {
     console.log(index, value);
-    value = value + 1;
     vet[index] = vet[index] + 2;
 });
 console.log(array);
+*/
+
+console.log(pessoas);
+
+pessoas.forEach((value, index, vet) => {
+    console.log(value.nome, value.idade);
+    value.idade++;
+    //vet[index].idade++;
+});
+
+console.log(pessoas);
