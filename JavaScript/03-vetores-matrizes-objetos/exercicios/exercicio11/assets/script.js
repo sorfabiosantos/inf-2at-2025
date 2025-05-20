db33408c36ec33af9eb2032f85a7f57079487681
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-05-20 15:36:31 -0300

    Exercício de prova

--- JavaScript/03-vetores-matrizes-objetos/exercicios/exercicio11/assets/script.js
+++ JavaScript/03-vetores-matrizes-objetos/exercicios/exercicio11/assets/script.js
@@ -2,14 +2,23 @@ const name = document.querySelector("#name");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const buttonAdd = document.querySelector("#add-user");
-let listUsers = [];
+let listUsers = [{
+    name: "Fábio Luís",
+    email: "fabiosantos@gmail.com",
+    password: "23456"
+    },
+    {
+        name: "Godofredo",
+        email: "godofredo@gmail.com",
+        password: "12345"
+    }];
 
 const listUsersUl = document.querySelector("#user-list");
 
 function showUsers () {
     listUsersUl.innerHTML = "";
     listUsers.forEach((value, index) => {
-        console.log(value.name, value.email, index)
+        //console.log(value.name, value.email, index)
         const li = document.createElement("li");
         li.setAttribute("userId", index);
         li.innerHTML = `
@@ -22,6 +31,8 @@ function showUsers () {
 
 
 }
+
+showUsers();
 
 buttonAdd.addEventListener("click", () => {
     listUsers.push(
@@ -36,3 +47,21 @@ buttonAdd.addEventListener("click", () => {
     showUsers();
 });
 
+listUsersUl.addEventListener("click", (event) => {
+    //console.log(event.target.tagName);
+    if(event.target.tagName == "BUTTON") {
+        //console.log(event.target.className);
+
+        console.log(event.target.parentElement.getAttribute("userId"));
+        const userId = event.target.parentElement.getAttribute("userId");
+        console.log(listUsers[userId]);
+
+        if(event.target.className == "edit") {
+            console.log("vamos editar");
+        }
+
+        if(event.target.className == "delete") {
+            console.log("vamos deletar");
+        }
+    }
+});
