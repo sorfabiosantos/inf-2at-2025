886b8f4367e9e7a91777c0d386df9f8e83e445c4
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-06-17 15:30:52 -0300

    refazer joão

---
+++ JavaScript/provas/2025/assets/script.js
@@ -0,0 +1,11 @@
+// Lista de objetos para armazenar os alunos
+let students = [
+    {
+        name: "Fábio",
+        grade: 10
+    },
+    {
+        name: "Godofredo",
+        grade: 7
+    }
+];
