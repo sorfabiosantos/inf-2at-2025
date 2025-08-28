d06a1a9f1b77aa4c87d8d6abbe99d11abf3a5aac
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-08-28 14:45:22 -0300

    Exercícios - PHP

--- php/exercicio21/user-hello.php
+++ php/exercicio21/user-hello.php
@@ -1,7 +1,21 @@
 <?php
 
+
+
+
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
 $lastname = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_SPECIAL_CHARS);
 $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
 
-var_dump($name, $lastname, $age);
+//var_dump($name, $lastname, $age);
+
+echo "<h1>Olá, {$name} {$lastname}! Você tem {$age} anos.</h1>";
+
+if($age >= 18) {
+    echo "<h2>Você é maior de idade.</h2>";
+} else {
+    echo "<h2>Você é menor de idade.</h2>";
+}
+?>
+
+<a href="index.html">Voltar</a>
